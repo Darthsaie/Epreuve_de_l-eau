1b352b5f1abe202282016ffb2a76edd5582f8ba6
Darthsaie <steven.nemtzoff@gmail.com>
2023-03-19 18:53:11 +0100

    Ajout des derniers exercices à la racine

---
+++ eau00.py
@@ -0,0 +1,9 @@
+# Générer toutes les combinaisons possibles de 3 chiffres différents
+combinaisons = []
+for i in range(10):
+    for j in range(i + 1, 10):
+        for k in range(j + 1, 10)
+        combinaisons.append(f"{i}{j}{k}")
+
+# Afficher les combinaisons
+print(", ".join(combinaisons))
