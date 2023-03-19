43fb47727d45f06f1a8ab39e3fab9d9a06eada6a
Darthsaie <steven.nemtzoff@gmail.com>
2023-03-19 19:02:20 +0100

    Ajout des derniers exercices à la racine

--- eau00.py
+++ eau00.py
@@ -2,8 +2,8 @@
 combinaisons = []
 for i in range(10):
     for j in range(i + 1, 10):
-        for k in range(j + 1, 10)
-        combinaisons.append(f"{i}{j}{k}")
+        for k in range(j + 1, 10):
+            combinaisons.append(f"{i}{j}{k}")
 
 # Afficher les combinaisons
 print(", ".join(combinaisons))
